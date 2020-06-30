3ffd0395cef35ab751abe07a9b942f3bf04140e0
Черный треугольник <67608733+Black-Triangle-code@users.noreply.github.com>
2020-06-30 09:56:01 +0300

    Update Sozdanie_Clienta.py

--- Sozdanie_Clienta.py
+++ Sozdanie_Clienta.py
@@ -1,29 +1,6 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from telethon import sync, events
-import requests
-import json
-import hashlib
-import time
-import re
 from telethon import TelegramClient
-import webbrowser
-import urllib.request
-import os
 import sqlite3
-
-class RunChromeTests():
-    def testMethod(self):
-        selenium_url = "http://localhost:4444/wd/hub"
-        caps = {'browserName': 'chrome'}
-        driver = webdriver.Remote(command_executor=selenium_url, desired_capabilities=caps)
-        driver.maximize_window()
-        driver.get(url_rec)
-        time.sleep(waitin + 10)
-        driver.close()
-        driver.quit()
+import time
 
 db = sqlite3.connect('Account.db')
 cur = db.cursor()
@@ -51,3 +28,6 @@ while(True):
     client.start()
     x = x + 1
     time.sleep(1)
+    if x == 32:
+        print("Aккаунты активированы!")
+        break
