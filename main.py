dc29083e1fb883a795fcf5629909c2202736e41e
stepaut <stepa1792@gmail.com>
2021-01-22 21:00:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,28 @@ import subprocess
 import sys
 from colorama import init, Fore, Back, Style
 import time
+import psutil
+from subprocess import check_output, CalledProcessError
 
-init()
+def getPIDs(process):
+       try:
+           pidlist = map(int, check_output(["pidof", process]).split())
+       except  CalledProcessError:
+           pidlist = []
+       print ('list of PIDs = ' + ', '.join(str(e) for e in pidlist))
+       return pidlist
 
 print(Style.BRIGHT + Fore.YELLOW)
 while (True):
-    print("Start LTC")
-    processLTC = subprocess.Popen([sys.executable, "bot_LTC.py"])
-    processLTC.wait()
-    print("Start DOGE")
-    processDOGE = subprocess.Popen([sys.executable, "bot_DOGE.py"])
-    processDOGE.wait()
+    pid1 = getPIDs("/usr/bin/python3 bot_DOGE.py")
+    pid2 = getPIDs("/usr/bin/python3 control.py")
+    pid3 = getPIDs("/usr/bin/python3 bot_LTC.py")
+    for p in pid1:
+        psutil.Process(p).terminate()
+    for p in pid2:
+        psutil.Process(p).terminate()
+    for p in pid3:
+        psutil.Process(p).terminate()
+
+    process = subprocess.Popen([sys.executable, "control.py"])
+    process.wait()
