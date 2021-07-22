0992308c2a688ddc82e569eb95609bb4ce52f62d
DONSIMON92 <semenische@yandex.com>
2021-07-22 21:05:57 +0600

    back module with variables

--- create_db.py
+++ create_db.py
@@ -1,7 +1,6 @@
-import os
 import sqlite3
 
-from dotenv import load_dotenv
+from config import phone, password, api_id, api_hash, activity, litecoin
 
 db = sqlite3.connect("Account.db")
 cur = db.cursor()
@@ -19,19 +18,11 @@ cur.execute(
 )
 db.commit()
 
-load_dotenv()
-Phone = os.getenv("PHONE")
-password = os.getenv("PASSWORD")
-Api_id = os.getenv("API_ID")
-Api_hash = os.getenv("API_HASH")
-Activity = os.getenv("ACTIVITY")
-Litecoin = os.getenv("LITECOIN")
-
 cur.execute(f"SELECT PHONE FROM Account WHERE PHONE = '{Phone}'")
 if cur.fetchone() is None:
     cur.execute(
         """INSERT INTO Account(PHONE, PASS, API_ID, API_HASH, ACTIVITY, LITECOIN) VALUES (?,?,?,?,?,?);""",
-        (Phone, password, Api_id, Api_hash, Activity, Litecoin),
+        (phone, password, api_id, api_hash, activity, litecoin),
     )
     db.commit()
     print("Зарегистрирован:")
